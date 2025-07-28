dd120fe3c8e7242d3a4a40c11d4877b23f3be30b
Cihad Aydemir <55869787+cihadaydemir@users.noreply.github.com>
2025-07-28 23:53:59 +0200

    chore: add tanstack query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,11 +2,14 @@ import "../global.css"
 
 import { db, expo_sqlite } from "@/db/db"
 import migrations from "@/drizzle/migrations"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { useMigrations } from "drizzle-orm/expo-sqlite/migrator"
 import { useDrizzleStudio } from "expo-drizzle-studio-plugin"
 import { Stack } from "expo-router"
 import { Suspense } from "react"
 import { ActivityIndicator, Text, View } from "react-native"
+
+const queryClient = new QueryClient()
 
 export default function RootLayout() {
 	const { success, error } = useMigrations(db, migrations)
@@ -30,10 +33,12 @@ export default function RootLayout() {
 
 	return (
 		<Suspense fallback={<ActivityIndicator size="large" />}>
-			<Stack screenOptions={{ headerShown: false }}>
-				<Stack.Screen name="(setup)" />
-				<Stack.Screen name="(tabs)" />
-			</Stack>
+			<QueryClientProvider client={queryClient}>
+				<Stack screenOptions={{ headerShown: false }}>
+					<Stack.Screen name="(setup)" />
+					<Stack.Screen name="(tabs)" />
+				</Stack>
+			</QueryClientProvider>
 		</Suspense>
 	)
 }
