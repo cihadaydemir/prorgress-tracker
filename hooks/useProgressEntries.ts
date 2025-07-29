2ceed43c0d92abf53bdf848c53084794c18ba308
Cihad Aydemir <55869787+cihadaydemir@users.noreply.github.com>
2025-07-29 12:52:05 +0200

    feat: add images to progress query

--- hooks/useProgressEntries.ts
+++ hooks/useProgressEntries.ts
@@ -1,12 +1,20 @@
 import { db } from "@/db/db"
-import { progressTable } from "@/db/schema"
 import { useQuery } from "@tanstack/react-query"
 
 export const useProgressEntries = () => {
 	return useQuery({
 		queryKey: ["progress"],
 		queryFn: async () => {
-			return await db.select().from(progressTable)
+			const progressEntriesWithImages = await db.query.progressTable.findMany({
+				with: {
+					images: true,
+				},
+			})
+
+			return progressEntriesWithImages.map((entry) => ({
+				...entry,
+				images: entry.images.map((image) => image.path),
+			}))
 		},
 	})
 }
